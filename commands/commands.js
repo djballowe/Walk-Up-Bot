68f198364f3e387c4f76fbbf82c208ddc09c6e77
djballowe <djballowe@gmail.com>
2022-08-12 13:36:46 -0700

    bot token and setup added

---
+++ commands/commands.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder, Routes } = require("discord.js");
+const { REST } = require("@discordjs/rest");
+const { clientId, guildId, token } = require("../config.json");
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("intro")
+    .setDescription("Greatest YouTuber alive"),
+].map((command) => command.toJSON());
+
+const rest = new REST({ version: "10" }).setToken(token);
+
+rest
+  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+  .then(() => console.log("Successfully registered application commands."))
+  .catch(console.error);
